07120d1b36c70b8db2dd4de89f018b01cb22a7b3
Giovanni Cicero <92113467+Walrus01@users.noreply.github.com>
2024-09-24 01:50:06 +0000

    Changed Listar

--- src/Componentes/Carro/index.js
+++ src/Componentes/Carro/index.js
@@ -1,24 +1,31 @@
 import axios from "axios";
 import '../../index.css';
-import {MdDelete} from 'react-icons/md'
+import { MdDelete } from 'react-icons/md';
 
-
-const Carro = ({valor, carregaCarros}) => {
+const Carro = ({ valor, carregaCarros }) => {
     return (
         <div className='card'>
-            <h3 className='titulo'>{valor.nome}</h3>
-            <p className='subtitulo'>Preço: {valor.preco}</p>
-            <p className='subtitulo'>Descrição: {valor.descricao}</p>
-            <img src={valor.foto} alt='Imagem do carro' className='imagem'></img>
-            <MdDelete className='icone' onClick={()=>remover(valor.id)}></MdDelete>
+            <h3 className='titulo'>{valor.marca} {valor.modelo}</h3>
+            <p className='subtitulo'>Ano: {valor.ano}</p>
+            <p className='subtitulo'>Cor: {valor.cor}</p>
+            <p className='subtitulo'>Preço: R$ {valor.preco}</p>
+            <p className='subtitulo'>Quilometragem: {valor.quilometragem} km</p>
+            <p className='subtitulo'>Tipo de Combustível: {valor.tipo_combustivel}</p>
+            <img src={valor.foto_url} alt='Imagem do carro' className='imagem' />
+            <MdDelete className='icone' onClick={() => remover(valor.id)} />
         </div>
-    )
+    );
 
-    function remover(id){
-        let api = "https://webimotores.onrender.com/"
+    function remover(id) {
+        let api = "https://webimotores.onrender.com/carros";
         axios.delete(`${api}/${id}`)
-            .then(()=>{
+            .then(() => {
+                alert("Carro removido com sucesso!");
                 carregaCarros();
+            })
+            .catch(err => {
+                console.error("Erro ao remover carro:", err);
+                alert("Erro ao remover o carro. Tente novamente.");
             });
     }
 }
